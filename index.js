64edc7b19bc382a84997fd71452bf3a879015931
Thibaud Lepretre <thibaud.lepretre@gmail.com>
2019-04-07 14:46:57 +0200

    Fix personal info

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.get('/', (request, response) => {
   // Change the name and email so we know who you are!
   let args = {
       data: {
-        "firstname":"Jon",
-        "lastname":"Snow",
-        "email":"know@nothi.ng",
+        "firstname":"Thibaud",
+        "lastname":"Lepretre",
+        "email":"thibaud.lepretre@gmail.com",
         "id":appID
       },
       headers: { "Content-Type": "application/json" }
